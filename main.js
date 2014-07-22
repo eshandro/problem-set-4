44f6807910d1f27d1ef20c4ecec8157d3e30034f
eshandro <erik843@gmail.com>
2014-07-22 14:13:44 -0600

    Finished problem 1

--- main.js
+++ main.js
@@ -3,17 +3,20 @@
 // Problem 1
 var addNumbers = function(str) {
 	var allNumbers = [0,1,2,3,4,5,6,7,8,9];
-	// var strList = str.split('');
 	var numbersFromString = [];
-
-	for(var i=0; i < str.length; i++) {
+	var newNumber = '';
+	
+	for(var i=0; i <= str.length; i++) {
 		if(str.charAt(i) in allNumbers) {
-			+numbersFromString.push(+str.charAt(i));
+			newNumber += str.charAt(i);				
+			console.log(newNumber);
 		}
 		else {
-			numbersFromString.push('-');
+			numbersFromString.push(+newNumber);
+			newNumber = '';
 		}
 	}
-
-	console.log(numbersFromString);
+	var answer =  _.reduce(numbersFromString, function(memo,num) { 
+		return memo + num; }, 0);
+	return answer;
 }
